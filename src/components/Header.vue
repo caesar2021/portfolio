8b1931bfc7ea8f192e185738da0d066626114536
pjay <trapezia@pm.me>
2022-07-25 07:10:37 +0800

    initial commit

---
+++ src/components/Header.vue
@@ -0,0 +1,17 @@
+<template>
+	<header class="absolute inset-x-0 top-0">
+      <div class="p-10 flex items-center justify-between">
+        <div class="flex items-center">
+          <h1 class="hidden md:inline-block">PJay</h1>
+        </div>
+        <div class="flex gap-5">
+          <a href="/about">
+            About
+          </a>
+          <a href="">
+            Skills
+          </a>
+        </div>
+      </div>
+    </header>
+</template>
