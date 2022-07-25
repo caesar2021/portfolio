1db64022ba10863e12f4a386c4896c933ca783d0
pjay <trapezia@pm.me>
2022-07-25 08:29:58 +0800

    make intro component and move all of its related elements

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -1,13 +1,9 @@
 <template>
 	<main class="flex justify-center items-center min-h-screen">
-      <div class="max-w-sm w-full font-semibold text-center">
-        <h3 class="text-4xl">Hello! I'm <span class="text-indigo-500 font-bold">PJay</span></h3>
-        <p>I fix and break websites</p>
-        <pjay-socials/>
-      </div>
+    	<pjay-intro/>
     </main>
 </template>
 
 <script setup>
-	import PjaySocials from '@/components/Socials.vue'
+	import PjayIntro from '@/components/Intro.vue'
 </script>
