5951fb2d8c1d9ef98ccfadfe53e65e7fa616520e
pjay <trapezia@pm.me>
2022-07-25 08:11:16 +0800

    change intro name's font weight class to bold

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -1,7 +1,7 @@
 <template>
 	<main class="flex justify-center items-center min-h-screen">
       <div class="max-w-sm w-full font-semibold text-center">
-        <h3 class="text-4xl">Hello! I'm <span class="text-indigo-500">PJay</span></h3>
+        <h3 class="text-4xl">Hello! I'm <span class="text-indigo-500 font-bold">PJay</span></h3>
         <p>I fix and break websites</p>
       </div>
     </main>
