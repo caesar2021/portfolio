c02daad1764f05b82a946edfa54e42afd02415e6
pjay <trapezia@pm.me>
2022-07-25 08:26:39 +0800

    add social links

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -3,6 +3,11 @@
       <div class="max-w-sm w-full font-semibold text-center">
         <h3 class="text-4xl">Hello! I'm <span class="text-indigo-500 font-bold">PJay</span></h3>
         <p>I fix and break websites</p>
+        <pjay-socials/>
       </div>
     </main>
 </template>
+
+<script setup>
+	import PjaySocials from '@/components/Socials.vue'
+</script>
