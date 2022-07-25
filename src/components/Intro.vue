5805547b2162f58c951800c8cdd3c73f0c33d7e8
pjay <trapezia@pm.me>
2022-07-25 14:15:44 +0800

    add vue-i18n and use moment js to calculate age

--- src/components/Intro.vue
+++ src/components/Intro.vue
@@ -3,8 +3,18 @@
 		<div class="flex justify-center ">
 			<div class="pb-3 w-full max-w-sm">
 				<h4 class="text-3xl">Intro</h4>
-				<p>My full name is Juan Luna, 25 year old from Makati City, Cebu. My passion for web development started when i'm 12 years old. I created my very first website and since then i explore and learn a lot of technologies. It's the reason why i choose IT in college. Even then, I choose not to pursue due to my family's financial instability.</p>
+				<p>{{ $t("introContent", { fullName, age, address }) }}</p>
 			</div>
 		</div>
 	</div>
 </template>
+
+<script setup>
+	
+	import moment from 'moment'
+
+	const fullName = import.meta.env.VITE_FULL_NAME
+	const address = import.meta.env.VITE_ADDRESS
+	const age = moment().diff( moment(import.meta.env.VITE_BIRTH_DATE), 'years')
+
+</script>
--- src/main.js
+++ src/main.js
@@ -1,12 +1,20 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import VueFullPage from 'vue-fullpage.js'
+import { createI18n } from 'vue-i18n'
 
 import './index.css'
 import 'vue-fullpage.js/dist/style.css'
 
+import messages from './i18n'
 
+const i18n = createI18n({
+	locale: "en",
+	fallbackLocale: "en",
+	messages
+})
 
 createApp(App)
 	.use(VueFullPage)
+	.use(i18n)
 	.mount('#app')
